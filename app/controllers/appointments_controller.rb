6dbdfc8153bb388ca76d4e890bce3ff7280be0fd
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-09-14 16:09:16 -0500

    Create controllers

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,10 @@
+class AppointmentsController < ApplicationController
+  def create
+  end
+
+  def index
+  end
+
+  def delete
+  end
+end
---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,10 @@
+class DoctorsController < ApplicationController
+  def create
+  end
+
+  def index
+  end
+
+  def delete
+  end
+end
