da0762d0f9507812eb341e443bbcd3867ce32073
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-09-14 15:24:35 -0500

    Add appointment validation

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,8 @@
 class Appointment < ApplicationRecord
+  validates :date, presence: true
+  validates :user_id, presence: true
+  validates :doctor_id, presence: true
+
   belongs_to :user
   belongs_to :doctor
 end
