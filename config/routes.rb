738a720c90ad42ca924567708d34388664fbc487
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-09-14 16:14:36 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  post 'appointments/create'
+  get 'appointments/index'
+  delete 'appointments/delete'
+  post 'doctors/create'
+  get 'doctors/index'
+  delete 'doctors/delete'
+  get 'users/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
