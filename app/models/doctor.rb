1558dc1ca0f3e06a9ea306d9d56d4e10234843fd
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-09-14 15:24:46 -0500

    Add doctor validation

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,8 @@
 class Doctor < ApplicationRecord
+  validates :name, presence: true
+  validates :specialization, presence: true
+  validates :experience, presence: true
+
   has_many :appointments
   has_many :users, through: :appointments
 end
