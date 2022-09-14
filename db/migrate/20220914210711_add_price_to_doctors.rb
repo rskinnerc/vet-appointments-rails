27fc99ccbf8e19e1172837ea92613974c6d874f0
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-09-14 16:08:40 -0500

    Update migrations

---
+++ db/migrate/20220914210711_add_price_to_doctors.rb
@@ -0,0 +1,5 @@
+class AddPriceToDoctors < ActiveRecord::Migration[7.0]
+  def change
+    add_column :doctors, :price, :float
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_14_195640) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_14_210747) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_14_195640) do
     t.datetime "date", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "city"
     t.index ["doctor_id"], name: "index_appointments_on_doctor_id"
     t.index ["user_id"], name: "index_appointments_on_user_id"
   end
@@ -30,6 +31,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_14_195640) do
     t.integer "experience"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "price"
   end
 
   create_table "users", force: :cascade do |t|
