ad66739fc54cfc12c7ab2abd37f13f977f3067af
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-09-14 15:13:04 -0500

    Set up relations between models

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,2 +1,4 @@
 class Doctor < ApplicationRecord
+  has_many :appointments
+  has_many :users, through: :appointments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :appointments
+  has_many :doctors, through: :appointments
 end
