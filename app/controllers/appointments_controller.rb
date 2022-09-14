e334ae9960a1c20180f7e75e2b989897a802dffa
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-09-14 17:21:56 -0500

    Create controllers

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,10 +1,25 @@
 class AppointmentsController < ApplicationController
   def create
+    @date = appointment_params[:date]
+    @appointment = Appointment.new(appointment_params[:user_id, :doctor_id, :city],
+                                   date: DateTime.new(@date[0], @date[1], @date[2], @date[3], @date[4], @date[5]))
+    render json: @appointment.errors.full_messages unless @appointment.save
+    render json: 'Succesfully created'
   end
 
   def index
+    @appointments = Appointment.where(user_id: appointment_params[:user_id])
+    render json: @appointments
   end
 
   def delete
+    Appointment.destroy(appointment_params[:id])
+    render json: 'Succesfully deleted'
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:id, :user_id, :doctor_id, :date, :city)
   end
 end
