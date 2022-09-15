247696a6cdb86df314c6ff246d15311eb081042e
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-09-15 10:38:32 -0500

    Linters

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,8 +1,10 @@
 class AppointmentsController < ApplicationController
   def create
     @date = appointment_params[:date]
-    @appointment = Appointment.new(user_id: appointment_params[:user_id], doctor_id: appointment_params[:doctor_id],
-                                   city: appointment_params[:city], date: DateTime.new(@date[0], @date[1], @date[2], @date[3], @date[4], @date[5]))
+    @appointment = Appointment.new(user_id: appointment_params[:user_id],
+                                   doctor_id: appointment_params[:doctor_id],
+                                   city: appointment_params[:city],
+                                   date: DateTime.new(@date[0], @date[1], @date[2], @date[3], @date[4], @date[5]))
     render json: @appointment.errors.full_messages unless @appointment.save
     render json: 'Succesfully created'
   end
