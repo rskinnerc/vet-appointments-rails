7e31dc1da38ef084ba0f416648d08c5aedf61f46
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-09-15 10:24:24 -0500

    Linters

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,6 +1,7 @@
 class DoctorsController < ApplicationController
   def create
-    @doctor = Doctor.new(name: doc_params[:name], specialization: doc_params[:specialization], experience: doc_params[:experience], price: doc_params[:price])
+    @doctor = Doctor.new(name: doc_params[:name], specialization: doc_params[:specialization],
+                         experience: doc_params[:experience], price: doc_params[:price])
     render json: @doctor.errors.full_messages unless @doctor.save
     render json: 'Succesfully created'
   end
