402889d1e334d99c69cc67f6dab3cf7e5ac883d0
Juan Rosario <jrosario19@hotmail.com>
2022-09-19 15:31:54 -0400

    Add validation for city in appointment model

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,6 +2,7 @@ class Appointment < ApplicationRecord
   validates :date, presence: true
   validates :user_id, presence: true
   validates :doctor_id, presence: true
+  validates :city, presence: true
 
   belongs_to :user
   belongs_to :doctor
