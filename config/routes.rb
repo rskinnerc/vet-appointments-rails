cad2a4162b5ac7f3cdcd37678e31ad0bd3744edc
Juan Rosario <jrosario19@hotmail.com>
2022-09-21 15:54:54 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   post 'appointments/create'
   get 'appointments/index'
   delete 'appointments/delete'
