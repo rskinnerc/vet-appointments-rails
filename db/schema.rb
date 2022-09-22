b24fd68b1dcdf6b629db3f31e8159350629613ed
Juan Rosario <jrosario19@hotmail.com>
2022-09-22 12:28:40 -0400

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_15_233659) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_22_152634) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,6 +60,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_15_233659) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.float "price"
+    t.text "description"
   end
 
   create_table "users", force: :cascade do |t|
