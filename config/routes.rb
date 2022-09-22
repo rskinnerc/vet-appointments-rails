dbe145f86881983e86a7d1ba968de080f1ba5d54
Juan Rosario <jrosario19@hotmail.com>
2022-09-22 12:31:39 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,14 @@ Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   post 'appointments/create'
-  get 'appointments/index'
+  get 'users/:user_id/appointments', to: 'appointments#index'
   delete 'appointments/delete'
   post 'doctors/create'
   get 'doctors/index'
   delete 'doctors/delete'
   post 'users/create'
+  
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
