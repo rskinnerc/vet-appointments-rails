98325222cf9d42c8154fb61796a70e398e5fad32
Juan Rosario <jrosario19@hotmail.com>
2022-09-22 12:32:21 -0400

    Add test for appointment index for documentarion

--- spec/requests/api/api_spec.rb
+++ spec/requests/api/api_spec.rb
@@ -39,12 +39,13 @@ RSpec.describe 'api/api', type: :request do
               experience: { type: :integer },
               price: { type: :float },
               photo: { type: :string },
+              description: { type: :string },
             },
             required: [ 'name','specialization','experience','price' ]
           }
     
           response '200', 'doctor created' do
-            let(:doctor) { { name: 'Juan', specialization:'Vet', experience: 5, price: 1.5} }
+            let(:doctor) { { name: 'Juan', specialization:'Vet', experience: 5, price: 1.5, description:'Description1'} }
             run_test!
           end
     
@@ -91,7 +92,7 @@ RSpec.describe 'api/api', type: :request do
           }
     
           response '200', 'doctor deleted' do
-            doctor = Doctor.create(name: 'Juan', specialization:'Vet', experience: 5, price: 1.5)
+            doctor = Doctor.create(name: 'Juan', specialization:'Vet', experience: 5, price: 1.5, description:'Description1')
             let(:doctor)  { { id: doctor.id} }
             
             run_test!
@@ -122,35 +123,31 @@ RSpec.describe 'api/api', type: :request do
     
           response '200', 'appointment created' do
             user = User.last
-            doctor = Doctor.create( name: 'Juan', specialization:'Vet', experience: 5, price: 1.5)
+            doctor = Doctor.create( name: 'Juan', specialization:'Vet', experience: 5, price: 1.5, description:'Description1')
             let(:appointment) { { user_id: user.id, doctor_id: doctor.id, city: 'Santo Domingo', date: '2022-09-15T16:19:51.466Z'} }
             run_test!
           end
     
            response '400', 'Appointment not found' do
-            doctor = Doctor.create( name: 'Juan', specialization:'Vet', experience: 5, price: 1.5)
+            doctor = Doctor.create( name: 'Juan', specialization:'Vet', experience: 5, price: 1.5, description:'Description1')
             user = User.create(name: 'Juan12')
               let(:appointment) { { user_id: user.id, doctor_id: doctor.id, date: "2022-09-15T14:53:59.443Z"} }
              run_test!
            end
         end
     end
-    path '/appointments/index' do
+    path '/users/{user_id}/appointments' do
 
         get 'Get the appointment' do
           tags 'Appointment'
           consumes 'application/json'
-          parameter name: :appointment, in: :body, schema: {
-            type: :object,
-            properties: {
-              user_id: { type: :integer }
-            },
-            required: [ 'user_id' ]
-          }
-          user = User.create(name:"Alejandro")
-          let(:appointment) { {user_id: user.id } }
+          parameter name: :user_id, in: :path, type: :integer
           
-          response '200', 'appointment found' do
+          
+          response '200', 'appointments found' do
+            user = User.last
+            puts user.name
+            let(:user_id) { user.id }  
             after do |example|
                 content = example.metadata[:response][:content] || {}
                 example_spec = {
@@ -182,9 +179,9 @@ RSpec.describe 'api/api', type: :request do
     
           response '200', 'Successfully deleted' do
             user = User.last
-            doctor = Doctor.create( name: 'Juan', specialization:'Vet', experience: 5, price: 1.5)
+            doctor = Doctor.create( name: 'Juan', specialization:'Vet', experience: 5, price: 1.5, description:'Description1')
             appointment = Appointment.create(user_id: user.id, doctor_id: doctor.id, city: 'Santo Domingo', date: '2022-09-15T16:19:51.466Z')
-            let(:appointment)  { { id: appointment.id} }
+            let(:appointment)  { { id: appointment.id } }
             
             run_test!
           end
